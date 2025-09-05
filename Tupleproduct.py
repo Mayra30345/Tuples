5d90e5eace36f96425603ddfab1bad7852cf3462
Mayra30345 <palmayra966@gmail.com>
2025-09-05 18:32:47 +0530

    Add files via upload

---
+++ Tupleproduct.py
@@ -0,0 +1,11 @@
+def multiple_tuple(nums):
+    temp=list(nums)
+    product=  1
+    for x in temp:
+        product*=x
+    return product
+nums=(4,3,2,2,-1,18)
+print("Original tuple: ")
+print(nums)
+print("Product-multiplying all the numbers of the said tuple: ",
+multiple_tuple(nums))
